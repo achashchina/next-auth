dc633149e95d6f98f34463785c215152ed938bb1
achashchina <anna200985@gmail.com>
2023-03-23 10:11:41 +0200

    changed main img

--- layout/layout.js
+++ layout/layout.js
@@ -5,7 +5,9 @@ export default function Layout({ children }) {
     <div className="flex h-screen bg-blue-400">
       <div className="m-auto bg-slate-50 rounded-md w-3/5 h-3/4 grid lg:grid-cols-2">
         <div className={styles.imgStyle}>
-          <div className={styles.cartoonImg}></div>
+          <div className={styles.cartoonImg}>
+            <img src="/assets/imgtransp.png"></img>
+          </div>
           <div className={styles.cloud_one}></div>
           <div className={styles.cloud_two}></div>
         </div>
